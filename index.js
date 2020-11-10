736504effad919e62028fefd1622cac32b476b49
kimchaeru <kmc280709@gmail.com>
2020-11-10 15:41:48 +0900

    add bot state

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.token;
 
-
 client.on('ready', () => {
-  console.log('켰다.');
-});
+    console.log('켰다.');
+    client.user.setPresence({ game: { name: '채루님!.' }, status: 'online' })
+  });
 
 let MuteRole = '768430566676496416'
 let Cooltime_Mute = 3 * 1000 //밀리세컨드 
